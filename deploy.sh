8b13ee9921a9e315677386b381f11eb5c7cdc676
O'Connell, Paul <Paul.O'Connell@mastercard.com>
2018-04-05 12:23:51 +0100

    Play with the pw.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 echo 'This is the deploy.script'
-echo 'FTP_USER     '.${FTP_USER}.'.'
-echo 'FTP_PASSWORD '.${FTP_PASSWORD}.'.'
-echo 'FTP_SITE     '.${FTP_SITE}.'.'
-echo 'FTP_PATH     '.${FTP_PATH}.'.'
-curl --list-only ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_SITE}/${FTP_PATH}
+echo 'FTP_USER     '${FTP_USER}
+echo 'FTP_PW1      '${FTP_PW1}
+echo 'FTP_PW2      '${FTP_PW2}
+echo 'FTP_PW3      '${FTP_PW3}
+PW = "<".${FTP_PW1}."%".${FTP_PW2}."%!".${FTP_PW3}
+echo 'PW           '$PW
+echo 'FTP_SITE     '${FTP_SITE}
+echo 'FTP_PATH     '${FTP_PATH}
+curl --list-only ftp://${FTP_USER}:$PW@${FTP_SITE}/${FTP_PATH}
