692d30bfdc6664abe9e73a107dd6b0472d41f198
paul.oconnell <paul.o'connell@mastercard.com>
2018-04-05 23:06:52 +0100

    If only one could spell. Fix typo's in the deployment file names.

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,12 @@ echo ftp://$1:$2@$3/$4
 curl --ftp-create-dirs -u "$1:$2" -T README.md                               ftp://$3/$4/README.md
 curl --ftp-create-dirs -u "$1:$2" -T src/Main.php                            ftp://$3/$4/src/Main.php
 curl --ftp-create-dirs -u "$1:$2" -T src/utils/Activator.php                 ftp://$3/$4/src/utils/Activator.php
-curl --ftp-create-dirs -u "$1:$2" -T src/utils/Dectivator.php                ftp://$3/$4/src/utils/Deactivator.php
+curl --ftp-create-dirs -u "$1:$2" -T src/utils/Deactivator.php               ftp://$3/$4/src/utils/Deactivator.php
 curl --ftp-create-dirs -u "$1:$2" -T vendor/autoload.php                     ftp://$3/$4/vendor/autoload.php
-curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/Classloader.php         ftp://$3/$4/vendor/composer/Classloader.php
+curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/ClassLoader.php         ftp://$3/$4/vendor/composer/ClassLoader.php
 curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/autoload_classmap.php   ftp://$3/$4/vendor/composer/autoload_classmap.php
-curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/autoload_namespace.php  ftp://$3/$4/vendor/composer/autoload_namespace.php
-curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/autoload_prs4.php       ftp://$3/$4/vendor/composer/autoload_prs4.php
+curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/autoload_namespaces.php ftp://$3/$4/vendor/composer/autoload_namespaces.php
+curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/autoload_psr4.php       ftp://$3/$4/vendor/composer/autoload_psr4.php
 curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/autoload_real.php       ftp://$3/$4/vendor/composer/autoload_real.php
 curl --ftp-create-dirs -u "$1:$2" -T vendor/composer/autoload_static.php     ftp://$3/$4/vendor/composer/autoload_static.php
 curl --ftp-create-dirs -u "$1:$2" -T bhaa_ee_plugin.php                      ftp://$3/$4/bhaa_ee_plugin.php
