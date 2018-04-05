4c8d6b5e6fd44047914b14219e9e18c43676dd6b
O'Connell, Paul <Paul.O'Connell@mastercard.com>
2018-04-05 12:30:49 +0100

    Pass params to the shell script.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 echo 'This is the deploy.script'
-echo 'FTP_USER     '${FTP_USER}
-echo 'FTP_PW1      '${FTP_PW1}
-echo 'FTP_PW2      '${FTP_PW2}
-echo 'FTP_PW3      '${FTP_PW3}
-PW = "<".${FTP_PW1}."%".${FTP_PW2}."%!".${FTP_PW3}
-echo 'PW           '$PW
-echo 'FTP_SITE     '${FTP_SITE}
-echo 'FTP_PATH     '${FTP_PATH}
-curl --list-only ftp://${FTP_USER}:$PW@${FTP_SITE}/${FTP_PATH}
+echo 'FTP_USER     '$1
+echo 'FTP_PASSWORD '$2
+echo 'FTP_SITE     '$3
+echo 'FTP_PATH     '$4
+echo ftp://$1:$2@$3/$4
+curl --list-only ftp://$1:$2@$3/$4
