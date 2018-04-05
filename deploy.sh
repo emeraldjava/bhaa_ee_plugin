41c080924fbdd403d3de91b0da429d11268c1909
paul.oconnell <paul.o'connell@mastercard.com>
2018-04-05 22:45:24 +0100

    Upload two files.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ echo 'FTP_PASSWORD '$2
 echo 'FTP_SITE     '$3
 echo 'FTP_PATH     '$4
 echo ftp://$1:$2@$3/$4
-curl --list-only -u "$1:$2" -v ftp://$3/$4
+curl --ftp-create-dirs -v -u "$1:$2" -T README.md ftp://$3/$4/README.md
+curl --ftp-create-dirs -v -u "$1:$2" -T src/Main.php ftp://$3/$4/src/Main.php
+curl --list-only -v -u "$1:$2" ftp://$3/$4
