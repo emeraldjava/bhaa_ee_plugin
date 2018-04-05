4d773f4e633d8780e5ede97a9dcbb5661e8f7fb4
O'Connell, Paul <Paul.O'Connell@mastercard.com>
2018-04-05 11:44:44 +0100

    First curl command.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo 'This is the deploy.script'
-echo 'FTP site '.${FPT_SITE}
+echo 'FTP site '.${FTP_SITE}
+curl --list-only ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_SITE}/${FTP_PATH}
