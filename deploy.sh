aea7d19c61d015bbc0a411027b04aac987cd074d
O'Connell, Paul <Paul.O'Connell@mastercard.com>
2018-04-05 11:57:23 +0100

    Check the character escaping.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 echo 'This is the deploy.script'
-echo 'FTP site '.${FTP_SITE}
+echo 'FTP_USER     '.${FTP_USER}.'.'
+echo 'FTP_PASSWORD '.${FTP_PASSWORD}.'.'
+echo 'FTP_SITE     '.${FTP_SITE}.'.'
+echo 'FTP_PATH     '.${FTP_PATH}.'.'
 curl --list-only ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_SITE}/${FTP_PATH}
